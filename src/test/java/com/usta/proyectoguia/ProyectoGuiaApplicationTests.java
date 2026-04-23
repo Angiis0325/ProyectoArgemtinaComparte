6a54185872f71cbbc7ca4d42fdaa3ab8b6e33719
katyc <angi.paez@usantoto.edu.co>
2026-04-23 14:06:05 -0500

    Inicial

---
+++ src/test/java/com/usta/proyectoguia/ProyectoGuiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usta.proyectoguia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGuiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
